70e858c5fce9f353e610b446d5a4ea339ea4c54d
Harry <harryaberg@gmail.com>
2021-07-07 17:27:39 +0000

    Deploys

--- 2. Introduction to ML/2. Validation and Testing/examples/train_test_split.py
+++ 2. Introduction to ML/2. Validation and Testing/examples/train_test_split.py
@@ -4,7 +4,7 @@ import numpy as np
 
 X, y = datasets.load_boston(return_X_y=True)
 
-X_train, y_train, X_test, y_test = model_selection.train_test_split(X, y)
+X_train, y_train, X_test, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
 model = linear_model.LinearRegression()
 
